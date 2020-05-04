0686bb8f45bf17d7535aa4dcb2c112e528ae60a6
Wagner Leonardi <leonardiwagner@gmail.com>
2020-05-03 21:28:08 -0300

    test deploy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('OK')
+  res.send('OK2')
 })
 
 app.listen(7000, () =>  console.log('listening on :7000'))
